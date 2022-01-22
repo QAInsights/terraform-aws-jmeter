21a5aa2a6b1d44e3ea8771aab5a7dfed8f80940e
NaveenKumar Namachivayam <catch.nkn@gmail.com>
2022-01-22 12:53:08 -0500

    Add Outputs
    Signed-off-by: NaveenKumar Namachivayam <catch.nkn@gmail.com>

--- main.tf
+++
@@ -1,9 +0,0 @@
-module "jmeter_server" {
-  source = "./modules/jmeter-and-plugins"
-
-  # required for jmeter-server
-  aws_ami           = "ami-001089eb624938d9f"
-  aws_instance_type = "t2.micro"
-  aws_key_name      = "terraform"
-  jmeter_version    = "5.4.2"
-}
